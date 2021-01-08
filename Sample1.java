768799b98900696db51a778b8ac154d099c1376b
Madhumitha-P <76841657+Madhumitha-P@users.noreply.github.com>
2021-01-08 13:57:14 +0530

    Update Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -4,7 +4,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		String finalVal = "";
 		int count = 1;
-		String input = "aabbccdefggef";
+		String input = "aabbccdefgg";
 		char[] inputList = input.toCharArray();
 		String LastVal = null;
 		for(int i=0; i<inputList.length-1;i++){
